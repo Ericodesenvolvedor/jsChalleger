fc9fcea6a15bac2b4de126f086e62dd5e0e3eb19
Ericodesenvolvedor <yuukisouma2003@gmail.com>
2022-12-17 19:13:42 -0300

    feat: #2 - assign a value to a variable

---
+++ js-basic/variables/assign-a-value.js
@@ -0,0 +1,11 @@
+/*
+    Here, we declare the variable num. But, it has no value yet. Assign a value to it and run the code.
+
+    let num;
+
+    console.log(num);
+*/
+
+let num;
+num = 5;
+console.log(num);
